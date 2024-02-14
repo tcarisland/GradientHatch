0c30e3c3b790a913751be1cec26c94350a64048b
tcarisland <tcarisland@gmail.com>
2024-02-14 21:15:49 +0100

    fixed bug caused by locale-sensitive code closes #39

--- GradientHatch.glyphsFilter/Contents/Resources/hatchmetrics.py
+++ GradientHatch.glyphsFilter/Contents/Resources/hatchmetrics.py
@@ -2,27 +2,33 @@ import objc
 
 
 class HatchMetrics():
-    descender = 0
-    ascender = 0
+    descender = 800
+    ascender = -200
 
     def __init__(self):
         super().__init__()
 
     @objc.python_method
-    def getMetricFromLayer(self, layer, metricName):
+    def getMetricFromLayer(self, layer, metricTypeEnumOrdinal):
         try:
             for metricValueName in layer.master.metricValues:
-                if layer.master.metricValues[metricValueName].title() == metricName:
+                if (layer.master.metricValues[metricValueName] is not None
+                        and layer.master.metricValues[metricValueName].metric is not None
+                        and layer.master.metricValues[metricValueName].metric.type == metricTypeEnumOrdinal):
                     return int(layer.master.metricValues[metricValueName].position)
         except TypeError:
             for metricValue in layer.master.metrics:
-                if metricValue.title() == metricName:
+                if (metricValue is not None
+                        and metricValue.metric is not None
+                        and metricValue.metric.type == metricTypeEnumOrdinal):
                     return int(metricValue.position)
 
     @objc.python_method
     def setGlyphHeightFromLayer(self, layer):
-        self.ascender = self.getMetricFromLayer(layer, 'Ascender')
-        self.descender = self.getMetricFromLayer(layer, 'Descender')
+        # GSMetricsTypeAscender = 1
+        self.ascender = self.getMetricFromLayer(layer, 1)
+        # GSMetricsTypeDescender = 7
+        self.descender = self.getMetricFromLayer(layer, 7)
 
     @objc.python_method
     def getHeight(self):
